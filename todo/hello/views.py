19f149db497fb789b6f505b2553ceff4bb6ae867
Patryk Wasik <paatryk.wasik@gmail.com>
2023-11-29 17:57:28 +0100

    Templates

--- todo/hello/views.py
+++ todo/hello/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello, Brian!")
@@ -12,4 +12,6 @@ def david(request):
     return HttpResponse("Hello, David!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
