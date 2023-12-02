72e91bca048193ebd5ee5e752dd0022374e0bcb5
Patryk Wasik <paatryk.wasik@gmail.com>
2023-12-02 16:02:41 +0100

    Forms

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,9 +1,31 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
     })
